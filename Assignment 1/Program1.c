dc5f871fce42f3fe37272e1505d5c2f5d9c499e0
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2023-07-14 13:41:29 +0530

    Program1.c

--- Assignment 1/Program1.c
+++ Assignment 1/Program1.c
@@ -1,1 +1,29 @@
-abc
+/*1. Write a program that accept file name from user and open that file. */
+
+#include<stdio.h>
+#include<fcntl.h>
+
+int main(int argc , char * argv[])
+{
+   int fd = 0;
+   
+   if(argc != 2)
+   {
+      printf("Invalid number of argument\n");
+      return -1;
+   }
+   
+   fd = open(argv[1], O_RDONLY);
+   
+   if(fd == -1)
+   {
+      printf("Unable to open file\n");
+      return -1;
+   }
+   else
+   {
+      printf("File opened successfully with fd : %d\n", fd);
+   }
+   
+   return 0;
+}
