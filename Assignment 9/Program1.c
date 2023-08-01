b8dbede038ffc26910750765ef8847d090249f15
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2023-08-01 22:43:16 +0530

    Program1.c

--- Assignment 9/Program1.c
+++ Assignment 9/Program1.c
@@ -1,1 +1,49 @@
-.
+/*1. Write a program which creates new process which is responsible to write all file names which are present on Desktop in demo file which should be newly created. */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+#include<dirent.h>
+
+int main()
+{
+   int iRet = 0;
+   int fd = 0;
+   DIR *dp = NULL;
+   struct dirent * entry = NULL;
+    
+   iRet = fork();
+   
+   if(iRet == 0)
+   {
+      printf("Inside child process\n");
+    
+      dp = opendir("dynamic");
+    
+      if(dp == NULL)
+      {
+        printf("Unable to open directory \n");
+        return -1;
+      }
+      
+      fd = creat("demo.txt", O_RDWR);
+      
+      while((entry = readdir(dp)) != NULL)
+      {
+         write(fd , entry->d_name, strlen(entry->d_name));
+         write(fd, " ", 1);
+      }
+      
+      close(fd);
+      closedir(dp);
+   
+    }
+    
+    return 0;
+    
+ }
+
