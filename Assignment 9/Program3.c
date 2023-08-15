f8ee0b3c222543f353ee8e2b10b2b8ff7e4035c0
RadhikaBerde <74779834+RadhikaBerde@users.noreply.github.com>
2023-08-15 19:10:12 +0530

    Update Program3.c

--- Assignment 9/Program3.c
+++ Assignment 9/Program3.c
@@ -1,26 +1,17 @@
 /* 3. Write a program which is used to print priority of process. */
 
 #include <stdio.h>
-#include <sys/time.h>
 #include <sys/resource.h>
 
 int main() 
 {
-    int priority = 0;
-    int pid = 0;
-    
-    pid = getpid();
+    int ret = 0;
  
     // Get the priority of the current process
-    priority = getpriority(PRIO_PROCESS, 0);
+    ret = getpriority(PRIO_PROCESS, 0);
 
-    if (priority == -1) 
-    {
-        printf("Error getting process priority");
-        return 1;
-    }
 
-    printf("Priority of the current process (PID %d) is: %d\n", pid, priority);
+    printf("Priority of the current process is: %d\n", ret);
 
     return 0;
 }
